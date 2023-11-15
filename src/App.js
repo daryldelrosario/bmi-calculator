832a298ebb4afa5090833dbb897d42c1ca231bc0
Daryl del Rosario <daryldelrosario.is@gmail.com>
2023-11-15 05:10:18 -0500

    update src/App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [weight, setWeight] = useState('');
+  const [height, setHeight] = useState('');
+  const [bmi, setBMI] = useState(null);
+
+  const calculateBMI = () => {
+    if (weight && height) {
+      const bmiValue = (weight / (height * height)).toFixed(2);
+      setBMI(bmiValue);
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>BMI Calculator</h1>
+      <div className="calculator">
+        <label>
+          Weight (kg):
+          <input
+            type="number"
+            value={weight}
+            onChange={(e) => setWeight(e.target.value)}
+          />
+        </label>
+        <label>
+          Height (m):
+          <input
+            type="number"
+            value={height}
+            onChange={(e) => setHeight(e.target.value)}
+          />
+        </label>
+        <button onClick={calculateBMI}>Calculate BMI</button>
+        {bmi && <p>Your BMI is: {bmi}</p>}
+      </div>
     </div>
   );
 }
